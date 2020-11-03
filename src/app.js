559156e7c7240ec06d6ac33dc370b6f8048b96d7
arjunshibu <arjunshibu1999@gmail.com>
2020-11-03 22:00:57 +0530

    added rate-limiting to all endpoints

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const rateLimit = require("express-rate-limit");
 require('./mongo/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -6,7 +7,13 @@ const fourOhFourRouter = require('./routers/404')
 
 const app = express()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 20
+  });
+
 app.use(express.json())
+app.use(limiter);
 app.use(userRouter)
 app.use(taskRouter)
 app.use(fourOhFourRouter)
